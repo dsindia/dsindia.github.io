f0568843bffb3c16f9fdb8c5972799354355c3c3
Sathvik Nair <sathviknair@berkeley.edu>
2017-08-28 12:35:30 -0700

    pics

--- js/team.js
+++ js/team.js
@@ -2,32 +2,32 @@ core_info = [
 	{
 		name_pos: "Pancham Yadav, Founder",
 		profile: "UC Berkeley 2020, Computer Science, Cognitive Science",
-		img: "images/pancham.png",
+		img: "images/Pancham_Yadav.jpg",
 	},
 	{
 		name_pos: "Sathvik Nair, Founder",
 		profile: "UC Berkeley 2020, Computer Science, Statistics", 
-		img: "images/sathvik.png"
+		img: "images/Sathvik_Nair.jpg"
 	},
 	{
 		name_pos: "Janaki Vivrekar, Director of Curriculum Development",
 		profile: "UC Berkeley 2020, Computer Science, Math",
-		img: 'images/janaki.png',
+		img: 'images/Janaki_Vivrekar.jpg',
 	},
 	{
 		name_pos: "Suraj Rampure, Director of Curriculum Development",
 		profile: "UC Berkeley 2020, Electrical Engineering and Computer Science, Engineering Math and Statistics",
-		img: "images/suraj.png",
+		img: "images/Suraj_Rampure.jpg",
 	},
 	{
 		name_pos: 'Rohit Shylakumar, Curriculum Developer and Bangalore Organizing Lead',
 		profile: 'City University of Hong Kong 2021, Computer Science, Creative Media',
-		img: 'images/rohit.png',
+		img: 'images/Rohit_Shylakumar.jpg',
 	},
 	{
 		name_pos: "Anoushka Bose, Curriculum Developer",
 		profile: "MIT 2020, Nuclear Science and Engineering",
-		img: 'images/anoushka.png',
+		img: 'images/Anoushka_Bose.jpg',
 	},
 	{
 		name_pos: "Yajushi Mattegunta, Curriculum Developer",
@@ -37,46 +37,47 @@ core_info = [
 	{
 		name_pos: "Shreyan Jain, Curriculum Developer",
 		profile: "MIT 2020, Computer Science, Math",
-		img: 'images/shreyan.png',
+		img: 'images/Shreyan_Jain.jpg',
 	},
 	{
 		name_pos: "Yuvraj Khetan, Curriculum Developer",
-		profile: "London School of Economics and Political Science 2019, Economics, Statistics"
-	}
+		profile: "London School of Economics and Political Science 2019, Economics, Statistics",
+		img: "images/Yuvraj_Khetan.jpg",
+	},
 	{
 		name_pos: "Batool Naqvi, Curriculum Developer",
 		profile: "UC Berkeley 2020, Computer Science",
-		img: "images/batool.png",
+		img: "images/Batool_Naqvi.jpg",
 	},
 	{
 		name_pos: "Ameet Rahane, Curriculum Developer",
 		profile: "UC Berkeley 2020, Computer Science, Cognitive Science",
-		img: 'images/ameet.png',
+		img: 'images/Ameet_Rahane.jpg',
 	},
 	{
 		name_pos: "Kairavi Sarup, Director of Corporate Outreach",
 		profile: "Tufts University 2020, International Relations, Archaeology",
-		img: "images/kai.png",
+		img: "images/Kairavi_Sarup.png",
 	},
 	{
 		name_pos: "Pranav Garg, Finance and Operations Officer",
 		profile: 'UC Berkley 2021, Business Administration',
-		img: "images/pranavg.png",
+		img: "images/Pranav_Garg.png",
 	},
 	{
 		name_pos: "Namita Verma, Corporate Outreach Officer",
 		profile: "UC Berkeley 2019, Economics, Psychology",
-		img: "images/namita.png",
-	}
+		img: "images/Namita_Verma.jpg",
+	},
 	{
 		name_pos: "Shyamsunder Sriram, Finance Officer",
 		profile: "University of Chicago 2020, Applied Math",
-		img: "images/shyam.png",
+		img: "images/Shyamsunder_Sriram.jpg",
 	},
 	{
 		name_pos: "Priyanshi Bareja, Graphic Designer",
 		profile: "Rhode Island School of Design 2020, Industrial Design",
-		img: 'images/priyanshi.png',
+		img: 'images/Priyanshi_Bareja.jpg',
 	},
 	{
 		name_pos: "Annie Tang, Graphic Designer",
@@ -85,18 +86,13 @@ core_info = [
 	},
 	{
 		name_pos: "Vinit Jain, Web Developer",
-		profile: "",
-		img: 'images/vinit.png',
+		profile: "Jain University 2017, Computer Science and Engineering",
+		img: 'images/Vinit_Jain.jpg',
 	},
 	{
 		name_pos: "Konrad Cheng, Web Developer",
 		profile: "University of Southern California 2021, Computer Science, Business Administration",
-		img: 'images/konrad.png',
-	},
-	{
-		name_pos: 'Siddharth Verma, Web Developer',
-		profile: "UC Berkeley 2021, Music, Computer Science",
-		img: 'images/siddharth.png',
+		img: 'images/Konrad_Cheng.jpg',
 	}
 ]
 
@@ -109,82 +105,82 @@ volunteer_info = [
 	{
 		name_pos: "Keshav Prasanna, Chennai Volunteer Instructor",
 		profile: "UCLA",
-		img: "images/keshav.png",
+		img: "images/Kesav_Prasanna.png",
 	},
 	{
 		name_pos: "Tarun Narayanan, Chennai Volunteer Instructor",
 		profile: "High School",
-		img: 'images/tarun.png',
+		img: 'images/Tarun_N.jpg',
 	},
 	{
 		name_pos: "Guru Nicketan, Chennai Volunteer Instructor",
 		profile: "",
-		img: 'images/guru.png',
+		img: 'images/Guru_Nicketan.jpg',
 	},
 	{
 		name_pos: "Sairandri Sathyanarayanan, Chennai Volunteer Instructor",
 		profile: "University of Washington",
-		img: 'images/sairandri.png',
+		img: 'images/Sairandri_S.png',
 	},
 	{
 		name_pos: "Yogesh Saravanan, Chennai Volunteer Instructor",
 		profile: "",
-		img: "images/yogesh.png",
+		img: "images/Yogesh_S.png",
 	},
 	{
 		name_pos: "Pranav Bhasin, Gurgaon and Delhi Organizing Lead",
 		profile: "UC Berkeley",
-		img: "images/pranavb.png",
+		img: "images/Pranav_Bhasin.png",
 	},
 	{
 		name_pos: "Sathya Sahay, Gurgaon Volunteer Instructor",
 		profile: "Ashoka University",
-		img: "images/sathya.png",
+		img: "images/Sathya_S.jpg",
 	},
 	{
 		name_pos: "Mehul Rastogi, Gurgaon Volunteer Instructor",
 		profile: "BITS Pilani, Goa",
-		img: "images/mehul.png",
+		img: "images/Mehul_R.jpg",
 	},
 	{
 		name_pos: "Damini Mehta, Gurgaon Volunteer Instructor",
 		profile: "Delhi University",
-		img: "images/damini.png",
+		img: "images/Damini_M.jpg",
 	},
 	{
 		name_pos: "Abhay Singh, Gurgaon Volunteer Instructor",
 		profile: "High School",
-		img: "images/abhay.png",
+		img: "images/Abhay_S.jpg",
 	},
 	{
 		name_pos: "Joshua Sabherwal, Gurgaon Volunteer Instructor",
 		profile: "High School",
-		img: "images/joshua.png",
+		img: "images/Joshua_S.jpg",
 	},
 	{
 		name_pos: "Ameek Singh, Delhi Volunteer Instructor",
 		profile: "High School",
-		img: "images/ameek.png",
+		img: "images/Ameek_Singh.png",
 	},
 	{
 		name_pos: "Sidhant Bhavnani, Delhi Volunteer Instructor, Web Developer",
 		profile: "High School",
-		img: "images/sidhant.png",
+		img: "images/Sidhant_Bavani.jpg",
 	},
 	{
 		name_pos: "Navin Muralidharan, Bangalore Volunteer Instructor",
-		profile: "City University of Hong Kong",
-		img: "images/navin.png",
+		profile: "Hong Kong University",
+		img: "images/Navin_M.png",
 	},
 	{
 		name_pos: "Soujanyo Ray Chaudhuri, Bangalore Volunteer Instructor",
 		profile: "City University of Hong Kong",
-		img: "images/soujanyo.png",
+		img: "images/Soujanyo.jpg",
 	},
 	{
 		name_pos: "Sumukh Swamy, Bangalore Volunteer Instructor",
-		profile: "",
-		img: "images/sumukh.png",
+		profile: "PESU Bangalore",
+		img: "images/Sumukh_S.jpg",
 	}
 ]
 
@@ -193,9 +189,9 @@ $(document).ready(function() {
 	    $('#core').append(
 	    	"<div class='profile'><div class='img-wrapper'>"+
 	              "<img src='"+ core_info[i].img + "'/></div>" +
-	         "<p>" + core_info[i].name_pos + "</p>")
+	         "<p>" + core_info[i].name_pos + "</p>" +
 	          "<p>" + core_info[i].profile + "</p></div>"
-	    )
+		)
 	}
 	for (var j = 0; j < volunteer_info.length; j++) {
 		$('#instructors').append(
